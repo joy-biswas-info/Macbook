f20a811f0e308dae1b0ae44f0b02fdd9a33bb54e
joy biswas <contact.joybiswas@gmail.com>
2021-08-21 09:37:23 +0600

    some attractive interaction addes

--- js/main.js
+++ js/main.js
@@ -20,59 +20,110 @@ function updateTotal() {
 };
 function promoCode() {
   const total = document.getElementById("total");
-  const input = document.getElementById('input');
+  const input = document.getElementById("input");
   const price = totalPrice();
   const inputValue = input.value;
-  if (inputValue == 'stevekaku') {
+  if (inputValue == "stevekaku") {
     total.innerText = price - (price / 100) * 20;
-    alert('Hurray You Have got 20% Discount')
-  }
-  else {
+    alert("Hurray You Have got 20% Discount");
+  } else {
     total.innerText = totalPrice();
   }
-}
-document.getElementById('8gb-ram').addEventListener('click', function () {
-    const ramCost = document.getElementById('ram-cost');
+};
+// To defain Selected Element
+function selecded(elem) {
+  return elem.classList.contains("selected");
+};
+
+const ram8Gb = document.getElementById("8gb-ram");
+ram8Gb.addEventListener("click", function () {
+  const ramCost = document.getElementById("ram-cost");
   ramCost.innerText = 0;
+  // defain selected Option
+    if (!selecded(ram8Gb)) {
+    ram8Gb.classList.add('selected');
+    ram16Gb.classList.remove('selected');
+  };
   totalPrice();
   updateTotal();
 });
-document.getElementById('16gb-ram').addEventListener('click', function () {
-   const ramCost = document.getElementById("ram-cost");
+const ram16Gb = document.getElementById("16gb-ram");
+ram16Gb.addEventListener("click", function () {
+  const ramCost = document.getElementById("ram-cost");
   ramCost.innerText = 180;
+  // defain selected Option
+  if (!selecded(ram16Gb)) {
+    ram16Gb.classList.add("selected");
+    ram8Gb.classList.remove("selected");
+  }
   totalPrice();
-  updateTotal();  
+  updateTotal();
 });
-document.getElementById("256gb-ssd").addEventListener("click", function () {
+
+const ssd256Gb = document.getElementById("256gb-ssd");
+ssd256Gb.addEventListener("click", function () {
   const storageCost = document.getElementById("storage-cost");
   storageCost.innerText = 0;
+  // defain selected Option
+  if (!selecded(ssd256Gb)) {
+    ssd256Gb.classList.add("selected");
+    ssd512Gb.classList.remove("selected");
+    ssd1Tb.classList.remove("selected");
+  }
   totalPrice();
   updateTotal();
 });
-document.getElementById("512gb-ssd").addEventListener("click", function () {
+
+const ssd512Gb = document.getElementById("512gb-ssd");
+ssd512Gb.addEventListener("click", function () {
   const storageCost = document.getElementById("storage-cost");
   storageCost.innerText = 100;
+  // defain selected Option
+  if (!selecded(ssd512Gb)) {
+    ssd512Gb.classList.add("selected");
+    ssd256Gb.classList.remove("selected");
+    ssd1Tb.classList.remove("selected");
+  }
   totalPrice();
   updateTotal();
 });
-document.getElementById("1tb-ssd").addEventListener("click", function () {
+const ssd1Tb = document.getElementById("1tb-ssd");
+ssd1Tb.addEventListener("click", function () {
   const storageCost = document.getElementById("storage-cost");
   storageCost.innerText = 180;
+  // defain selected Option
+  if (!selecded(ssd1Tb)) {
+    ssd512Gb.classList.remove("selected");
+    ssd256Gb.classList.remove("selected");
+    ssd1Tb.classList.add("selected");
+  };
   totalPrice();
   updateTotal();
 });
-document.getElementById("free-delivery").addEventListener("click", function () {
-  const deliveryCost = document.getElementById("delivery-cost");
-  deliveryCost.innerText = 0;
-  totalPrice();
-  updateTotal();
-});
-document.getElementById("express-delivery").addEventListener("click", function () {
-  const deliveryCost = document.getElementById("delivery-cost");
-  deliveryCost.innerText = 20;
-  totalPrice();
-  updateTotal();
-});
+
+const freeDelivery = document.getElementById("free-delivery");
+  freeDelivery.addEventListener("click", function () {
+    const deliveryCost = document.getElementById("delivery-cost");
+    deliveryCost.innerText = 0;
+    if (!selecded(freeDelivery)) {
+      freeDelivery.classList.add("selected");
+      expressDelivery.classList.remove("selected");
+    };
+    totalPrice();
+    updateTotal();
+  });
+
+const expressDelivery = document.getElementById("express-delivery");
+    expressDelivery.addEventListener("click", function () {
+      const deliveryCost = document.getElementById("delivery-cost");
+      deliveryCost.innerText = 20;
+      if (!selecded(expressDelivery)) {
+      freeDelivery.classList.remove("selected");
+      expressDelivery.classList.add("selected");
+    };
+      totalPrice();
+      updateTotal();
+    });
 document.getElementById('promo').addEventListener('click',function () {
 promoCode();
 })
